090da1c637ad6333e03b0d54246f15844a917137
Franco D. Berdun <fberdun@gmail.com>
2020-03-25 03:44:29 +0100

    add state, API fetch, select element

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import React, { Component } from 'react';
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 class App extends Component {
+  _isMounted = false;
+  state = {
+    isLoading: true,
+    users: [],
+    user: undefined
+  }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
   componentDidMount() {
+    this._isMounted = true;
+    const URL = 'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+    fetch(URL, { mode: 'cors' })
+      .then(res => res.json())
+      .then((data) => {
+        if (this._isMounted) {
+          this.setState({ isLoading: false })
+          this.setState({ users: data["Brastlewark"] })
+        }
+      })
+      .catch(console.log)
   }
 
   componentWillUnmount() {
+    this._isMounted = false;
   }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   render() {
@@ -30,6 +40,31 @@ class App extends Component {
             <h4> Pasiona Front Test </h4>
           </div>
         </div>
+
+
+          <div className="row">
+            <div className="col-12">
+              <div className="searchField">
+                <div className="body">
+                  <div className="form col-6">
+                    <form className="search-box">
+                      Select Gnome
+                      <select className='form-control'>
+                        <option className="field" value=""> - </option>
+                        {
+                          this.state.users.map(user => {
+                            return <option key={user.id + user.name} className="field" value={user.id}>{user.name}</option>
+                          })
+                        }
+                      </select>
+                    </form>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
 
 
     </div>
